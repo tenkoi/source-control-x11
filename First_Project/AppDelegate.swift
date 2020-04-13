943d301a8424fd0b699602ce309e0dbfd738330a
Faiz <tenkois@gmail.com>
2020-04-14 00:36:52 +0800

    Initial Commit

---
+++ First_Project/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  First_Project
+//
+//  Created by Faiz Alias on 14/04/2020.
+//  Copyright © 2020 Tenkoi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ First_Project/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  First_Project
+//
+//  Created by Faiz Alias on 14/04/2020.
+//  Copyright © 2020 Tenkoi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
