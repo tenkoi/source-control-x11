05a8565e776ad7861c526efe9063ccd65961f5b2
Faiz <tenkois@gmail.com>
2020-04-14 02:20:19 +0800

    Add AddNumbers method

--- First_Project/ViewController.swift
+++ First_Project/ViewController.swift
@@ -15,6 +15,17 @@ class ViewController: NSViewController {
 
         // Do any additional setup after loading the view.
     }
+}
+/**
+ Adds two numbers togather and return the result.
+ - parameter num1: The first number.
+ - parameter num2: The second number.
+ - returns: The sum of num1 and num2.
+ */
+
+func addNumbers(num1: Int, num2: Int) -> Int {
+    return num1 + num2
+}
 
     override var representedObject: Any? {
         didSet {
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+var str = "Hello, playground"
+str = " Changed!"
+var num = 1
+num += 1
+for i in 0...10 {
+    num += i
+}
+num
